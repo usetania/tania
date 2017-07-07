247cf807c81c71d8971dd3a6dd88ff18934428f5
Asep Bagja Priandana <bepitulaz@gmail.com>
2017-07-07 09:32:09 +0700

    Changing assets and cleaning unused assets.
    I'm replacing the logo with the new one and deleting plenty of unused
    javascript scripts.

--- web/assets/js/template.js
+++
@@ -1,8 +0,0 @@
-/**
- * This file only for template development.
- * Making partial template with jQuery.
- */
-$(document).ready(function() {
-    $("#header").load("header.html");
-    $("#sidebar").load("sidebar.html");
-});

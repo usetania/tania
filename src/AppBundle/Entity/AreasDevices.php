a442ad12c6b08911974279f33fdc7de240e379f3
Asep Bagja Priandana <bepitulaz@gmail.com>
2017-09-18 23:03:45 +0700

    WIP Creating form to add new device
    I'm creating new device type class and this class will be used for
    creating new device form.

--- src/AppBundle/Entity/AreasDevices.php
+++ src/AppBundle/Entity/AreasDevices.php
@@ -46,4 +46,110 @@ class AreasDevices
     * @Assert\Type("\DateTime")
     */
     private $createdAt;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return AreasDevices
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return AreasDevices
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set area
+     *
+     * @param \AppBundle\Entity\Area $area
+     *
+     * @return AreasDevices
+     */
+    public function setArea(\AppBundle\Entity\Area $area = null)
+    {
+        $this->area = $area;
+
+        return $this;
+    }
+
+    /**
+     * Get area
+     *
+     * @return \AppBundle\Entity\Area
+     */
+    public function getArea()
+    {
+        return $this->area;
+    }
+
+    /**
+     * Set device
+     *
+     * @param \AppBundle\Entity\Device $device
+     *
+     * @return AreasDevices
+     */
+    public function setDevice(\AppBundle\Entity\Device $device = null)
+    {
+        $this->device = $device;
+
+        return $this;
+    }
+
+    /**
+     * Get device
+     *
+     * @return \AppBundle\Entity\Device
+     */
+    public function getDevice()
+    {
+        return $this->device;
+    }
 }
